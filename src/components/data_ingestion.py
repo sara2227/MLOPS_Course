e13ba2562ab434b9787933dafacc8da90ce00bb5
sara2227 <sarasaberi.mt@gmail.com>
2024-01-14 19:07:16 +0100

    model_trainer is completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,7 @@ import pandas as pd
 from dataclasses import dataclass
 from sklearn.model_selection import train_test_split
 from src.components.data_transformation import DataTransformation,DataTransformationConfig
+from src.components.model_trainer import ModelTrainer
 
 '''
     This file is related to take data from databases or csv files, it handels train test split data
@@ -51,5 +52,9 @@ if __name__ == '__main__':
     obj = DataIngestion()
     train_data_path,test_data_path = obj.initiate_data_ingestion()
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
+    modeltrainer = ModelTrainer()
+    modeltrainer.initiate_model_trainer(train_arr,test_arr)
+
 
