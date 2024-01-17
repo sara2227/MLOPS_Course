765630354a62a506454f2b7ac7d15df951999af4
sara2227 <sarasaberi.mt@gmail.com>
2024-01-17 12:07:48 +0100

    delete app.py and add application.py

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -86,12 +86,11 @@ class DataTransformation:
             train_arr = np.c_[input_feature_train_arr,np.array(target_feature_train_df)]
             test_arr = np.c_[input_feature_test_arr,np.array(target_feature_test_df)]
 
-            logging.info(f"Saved preprocessing object.")
-
             save_object(
                 file_path = self.data_transformation_config.preprocessor_obj_file_path,
                 obj = preprocessing_obj
             )
+            logging.info(f"Saved preprocessing object.")
 
             return(
                 train_arr,
