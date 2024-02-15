99969ca28a062709d2429421ebcbabce27960c00
sara2227 <sarasaberi.mt@gmail.com>
2024-02-15 12:05:20 +0100

    change application file for deployment

--- application.py
+++ application.py
@@ -41,4 +41,5 @@ def predict_datapoint():
         return render_template('home.html',results = results[0])
 
 if __name__ == '__main__':
-    app.run(debug = True,host='0.0.0.0',port=5000)
+    # app.run(debug = True,host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0') # when we want to deploy, turn off debug mode
